a26249a1a6831c7216d422317b90c1edf015ed53
XiaoBo <chenxiaobo@xmfunny.com>
2023-06-09 10:39:16 +0800

    feat: 新增外部 API 调用样版代码

---
+++ Core/FunnyAPI/Funny.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SoFunny.FunnySDK.Internal;
+using SoFunny.FunnySDK.UIModule;
+
+namespace SoFunny.FunnySDK
+{
+    public static partial class Funny
+    {
+        internal static class Bridge
+        {
+            private static BridgeService bridgeService;
+
+            internal static BridgeService Service
+            {
+                get
+                {
+                    if (bridgeService == null)
+                    {
+                        throw new InvalidOperationException("FunnySDK 未初始化，请先调用 Initialize 方法。");
+                    }
+                    return bridgeService;
+                }
+            }
+
+            internal static void Init()
+            {
+                if (bridgeService != null) { return; }
+
+                bridgeService = new BridgeService(ConfigService.Config.AppID, ConfigService.Config.IsMainland);
+            }
+        }
+
+        /// <summary>
+        /// FunnySDK 核心初始化
+        /// </summary>
+        public static void Initialize()
+        {
+            // 初始化桥接服务对象
+            Bridge.Init();
+            // 调用桥接初始化方法
+            Bridge.Service.Call(Method.Initialize);
+        }
+    }
+}
+
---
+++ Core/FunnyAPI/FunnyLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SoFunny.FunnySDK
+{
+    public partial class Funny
+    {
+        public static class Login
+        {
+            public static void Start()
+            {
+                // 待定
+                Bridge.Service.Call("Test");
+            }
+        }
+    }
+}
+
