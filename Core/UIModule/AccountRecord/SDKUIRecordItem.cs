96db8d23171290d8daf9f5a8c85437d9dd0519df
XiaoBo <chenxiaobo@xmfunny.com>
2023-09-04 18:25:45 +0800

    feat: 新增账号登录记录功能

---
+++ Core/UIModule/AccountRecord/SDKUIRecordItem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace SoFunny.FunnySDK.UIModule
+{
+    public class SDKUIRecordItem : MonoBehaviour
+    {
+        [SerializeField]
+        private Text titleLabel;
+        [SerializeField]
+        private Button selectButton;
+        [SerializeField]
+        private Button removeButton;
+
+        public UnityAction<string> onSelectedAction;
+
+        public UnityAction<SDKUIRecordItem, string> onRemoveAction;
+
+        public void Setup(string title)
+        {
+            titleLabel.text = title;
+
+            selectButton.onClick.AddListener(OnSelectMe);
+            removeButton.onClick.AddListener(OnRemoveMe);
+        }
+
+        private void UnLoad()
+        {
+            selectButton.onClick.RemoveAllListeners();
+            removeButton.onClick.RemoveAllListeners();
+
+            onSelectedAction = null;
+            onRemoveAction = null;
+
+            Destroy(gameObject);
+        }
+
+        private void OnRemoveMe()
+        {
+            onRemoveAction?.Invoke(this, titleLabel.text);
+
+            UnLoad();
+        }
+
+        private void OnSelectMe()
+        {
+            onSelectedAction?.Invoke(titleLabel.text);
+        }
+
+    }
+}
+
+
---
+++ Core/UIModule/Utils/SFInputRecordScrollView.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+namespace SoFunny.FunnySDK.UIModule
+{
+    [Serializable]
+    public class StringEvent : UnityEvent<string> { }
+
+    public class SFInputRecordScrollView : MonoBehaviour, IPointerClickHandler
+    {
+        public GameObject scrollContent;
+        public GameObject itemPerfab;
+
+        public StringEvent onSelectRecord;
+        public UnityEvent onRemoveItem;
+
+        private List<SDKUIRecordItem> contentList = new List<SDKUIRecordItem>();
+
+        private void Awake()
+        {
+
+        }
+
+        public void Show()
+        {
+            var recordList = FunnyDataStore.GetAccountHistory();
+
+            foreach (var item in recordList)
+            {
+                var perfab = Instantiate(itemPerfab);
+                var recordItem = perfab.GetComponent<SDKUIRecordItem>();
+
+                recordItem.Setup(item);
+
+                recordItem.onRemoveAction = (target, value) =>
+                {
+                    FunnyDataStore.RemoveAccount(value);
+                    contentList.Remove(target);
+
+                    if (contentList.Count <= 0)
+                    {
+                        Hide();
+                    }
+
+                    onRemoveItem?.Invoke();
+                };
+
+                recordItem.onSelectedAction = (value) =>
+                {
+                    onSelectRecord?.Invoke(value);
+                    Hide();
+                };
+
+                perfab.transform.SetParent(scrollContent.transform);
+                perfab.transform.localScale = Vector3.one;
+                perfab.SetActive(true);
+
+                contentList.Add(recordItem);
+            }
+
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+
+            foreach (var item in contentList)
+            {
+                Destroy(item.gameObject);
+            }
+
+            contentList.Clear();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Hide();
+        }
+
+    }
+}
+
